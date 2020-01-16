dd341f4e262697e431b30935c51c29e32399bb4a
Casper Rogild Storm <casper+github@rogildstorm.com>
2020-01-16 15:24:36 +0100

    moved away from alacritty - lacking ligature fonts was a dealbreaker

---
+++ casks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source ./helpers.sh
+
+cask_dependency visual-studio-code
+cask_dependency iterm2
+cask_dependency alfred
+cask_dependency qlstephen
+cask_dependency brave-browser
+cask_dependency spotify
+cask_dependency protonvpn
+cask_dependency font-fira-code
--- install.sh
+++ install.sh
@@ -1,36 +1,5 @@
 #!/bin/bash
-
-function symlink {
-    CURRENT_REALPATH=$(realpath -m "$2")
-    TARGET_REALPATH=$(realpath -m "$1")
-    if [[ "$CURRENT_REALPATH" != "$TARGET_REALPATH" ]]; then
-        if [[ ! -f "$2" ]] || [[ ! -d "$2" ]]; then
-            rm -rf "$2"
-        fi
-        ln -sf $TARGET_REALPATH $CURRENT_REALPATH
-    fi
-}
-
-function dependency {
-    which -s $1
-    if [[ $? != 0 ]] ; then
-        echo "Installing $1..."
-        brew install $1
-    else
-        echo "Updating $1..."
-        brew upgrade $1
-    fi
-}
-
-function cask_dependency {
-    if ! brew cask info $1 &>/dev/null; then
-        echo "Installing $1..."
-        brew cask install $1
-    else
-        echo "Updating $1..."
-        brew cask upgrade $1
-    fi
-}
+source ./helpers.sh
 
 # Making sure we have Xcode CLI installed
 xcode-select --install
@@ -47,6 +16,7 @@ fi
 dependency fzf
 dependency z
 dependency asdf
+dependency yarn
 
 dependency yabai
 mkdir -p ~/.config/yabai
@@ -58,14 +28,10 @@ symlink ./files/tmux.conf ~/.config/.tmux.conf
 git clone git@github.com:goronfreeman/nord-tmux.git ~/.config/tmux/themes/nord-tmux
 # Note: using a fork, because as of writing this (2020-01-16) org repo does not support +2.9 tmux.
 
-
-cask_dependency alacritty
-mkdir -p ~/.config/alacritty
-symlink ./files/alacritty.yml ~/.config/alacritty/alacritty.yml
-
-cask_dependency visual-studio-code
-# todo: config / symlink
-
 dependency zsh
+dependency zsh-completions 
+dependency zsh-syntax-highlighting 
+dependency zsh-autosuggestions
+chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 symlink ./files/zshrc ~/.zshrc
