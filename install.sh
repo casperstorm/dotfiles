06392608ebe0e100b078454c7f17e11f6848731f
Casper <crs@async.local>
2020-01-30 19:52:42 +0100

    removed some strange folder ref

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# Making sure we have Xcode CLI installed
-xcode-select --install
-
-# Making sure we have Homebrew
 which -s brew
 if [[ $? != 0 ]] ; then
     echo "Installing homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-source ./casks.sh
-source ./formulae.sh
+brew tap koekeishiya/formulae
+
+brew install fzf
+brew install z
+brew install yarn
+brew install yabai
+brew install koekeishiya/formulae/skhd
+brew install zsh
+brew install zsh-completions 
+brew install zsh-syntax-highlighting 
+brew install zsh-autosuggestions
+
+brew cask install alacritty	
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
---
+++ symlink.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function symlink {
+    CURRENT_REALPATH=$(realpath -m "$2")
+    TARGET_REALPATH=$(realpath -m "$1")
+    if [[ "$CURRENT_REALPATH" != "$TARGET_REALPATH" ]]; then
+        if [[ ! -f "$2" ]] || [[ ! -d "$2" ]]; then
+            rm -rf "$2"
+        fi
+        ln -sf $TARGET_REALPATH $CURRENT_REALPATH
+    fi
+}
+
+# Making sure we have config folder
+mkdir -p ~/.config
+mkdir -p ~/.config/asdf
+mkdir -p ~/.config/yabai
+mkdir -p ~/.config/skhd
+mkdir -p ~/.config/tmux
+mkdir -p ~/.config/tmux/themes
+mkdir -p ~/.config/alacritty	
+
+# symlinking
+symlink ./files/alacritty.yml ~/.config/alacritty/alacritty.yml
+symlink ./files/asdfrc ~/.config/asdf/asdfrc
+symlink ./files/yabairc ~/.config/yabai/yabairc
+symlink ./files/skhdrc ~/.config/skhd/skhdrc
+symlink ./files/tmux.conf ~/.config/tmux/.tmux.conf
+symlink ./files/zshrc ~/.zshrc
