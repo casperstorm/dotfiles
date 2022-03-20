353d5ce017d859cec621874e8d048eaf5255f46c
Casper Rogild Storm <casper+github@rogildstorm.com>
2022-03-20 08:44:23 +0100

    enabled tmux integration to vim to enable seemless navigation

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -181,6 +181,23 @@ return require('packer').startup(function(use)
     end
   }
 
+  -- tmux
+  use({
+      "aserowy/tmux.nvim",
+      config = function()
+          require("tmux").setup({
+              navigation = {
+                  -- enables default keybindings (C-hjkl) for normal mode
+                  enable_default_keybindings = true,
+              },
+              resize = {
+                  -- enables default keybindings (A-hjkl) for normal mode
+                  enable_default_keybindings = true,
+              }
+          })
+      end
+  })
+
   if packer_bootstrap then
     require('packer').sync()
   end
