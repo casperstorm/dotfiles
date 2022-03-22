8054d6ee89a2e578a4d61ea54a8db096cfedf161
Casper Rogild Storm <casper+github@rogildstorm.com>
2022-03-22 20:42:04 +0100

    removed which-key for operators, motions, g and z.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -79,8 +79,12 @@ return require('packer').startup(function(use)
           plugins = {
               marks = true, -- shows a list of your marks on ' and `
               registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-              -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-              -- No actual key bindings are created
+              presets = {
+                  operators = false,
+                  motions = false,
+                  g = false,
+                  z = false,
+              },
           },
           window = {
               border = "single", -- none, single, double, shadow
