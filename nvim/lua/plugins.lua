14ef975cd36a495473fd9e62e196ced84337c101
Casper Rogild Storm <2248455+casperstorm@users.noreply.github.com>
2022-08-08 11:53:58 +0200

    re-added vsnip.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -19,6 +19,8 @@ return require("packer").startup(function(use)
     use "hrsh7th/cmp-buffer"
     use "hrsh7th/cmp-path"
     use "hrsh7th/cmp-cmdline"
+    use "hrsh7th/cmp-vsnip"
+    use "hrsh7th/vim-vsnip"
 
     -- themes
     use { 'glepnir/zephyr-nvim', requires = { 'nvim-treesitter/nvim-treesitter', opt = true }}
