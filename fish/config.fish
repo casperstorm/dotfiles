094ca6b8e4c22087532c5374083b1e57a2d6bfab
Casper Rogild Storm <2248455+casperstorm@users.noreply.github.com>
2022-08-24 10:56:58 +0200

    updated fish and tmux

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,9 @@
-# plugins
-zoxide init fish | source
-
 # paths
 fish_add_path $HOME/.cargo/bin
 fish_add_path /opt/homebrew/bin
+
+# plugins
+zoxide init fish | source
 
 # alias
 alias t="tmux attach || tmux new-session"
