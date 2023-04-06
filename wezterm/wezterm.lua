a116212191449c8f36e22695c3d99f62b8667686
Casper Rogild Storm <casper+github@rogildstorm.com>
2023-04-06 19:22:21 +0200

    Updated dots

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,73 +1,71 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.color_scheme = 'rose-pine'
+config.color_scheme = 'Kanagawa (Gogh)'
 config.font = wezterm.font 'Iosevka Term'
 config.font_size = 13.0
 config.window_decorations = "RESIZE"
 
 config.window_frame = {
-  font = wezterm.font { family = 'Iosevka Term', weight = 'Bold' },
-  font_size = 12.0,
+    font = wezterm.font {
+        family = 'Iosevka Term',
+        weight = 'Bold'
+    },
+    font_size = 12.0
 }
 
-
-config.keys = {
-  {
-    key = 'LeftArrow', mods = 'CTRL',
+config.keys = {{
+    key = 'LeftArrow',
+    mods = 'CTRL',
     action = wezterm.action.SplitPane {
-      direction = 'Left'
-    },
-  },
-  {
-    key = 'k', mods = 'CTRL|SHIFT',
+        direction = 'Left'
+    }
+}, {
+    key = 'k',
+    mods = 'CTRL|SHIFT',
     action = wezterm.action.SplitPane {
-      direction = 'Up'
-    },
-  },
-  {
-    key = 'l', mods = 'CTRL|SHIFT',
+        direction = 'Up'
+    }
+}, {
+    key = 'l',
+    mods = 'CTRL|SHIFT',
     action = wezterm.action.SplitPane {
-      direction = 'Right'
-    },
-  },
-  {
-    key = 'h', mods = 'CTRL|SHIFT',
+        direction = 'Right'
+    }
+}, {
+    key = 'h',
+    mods = 'CTRL|SHIFT',
     action = wezterm.action.SplitPane {
-      direction = 'Left'
-    },
-  },
-  {
-    key = 'j', mods = 'CTRL|SHIFT',
+        direction = 'Left'
+    }
+}, {
+    key = 'j',
+    mods = 'CTRL|SHIFT',
     action = wezterm.action.SplitPane {
-      direction = 'Down'
-    },
-  },
-    {
+        direction = 'Down'
+    }
+}, {
     key = 'w',
     mods = 'CTRL',
-    action = wezterm.action.CloseCurrentPane { confirm = true },
-  },
-  {
+    action = wezterm.action.CloseCurrentPane {
+        confirm = true
+    }
+}, {
     key = 'h',
     mods = 'CTRL',
-    action = wezterm.action.ActivatePaneDirection 'Left',
-  },
-  {
+    action = wezterm.action.ActivatePaneDirection 'Left'
+}, {
     key = 'j',
     mods = 'CTRL',
-    action = wezterm.action.ActivatePaneDirection 'Down',
-  },
-  {
+    action = wezterm.action.ActivatePaneDirection 'Down'
+}, {
     key = 'k',
     mods = 'CTRL',
-    action = wezterm.action.ActivatePaneDirection 'Up',
-  },
-  {
+    action = wezterm.action.ActivatePaneDirection 'Up'
+}, {
     key = 'l',
     mods = 'CTRL',
-    action = wezterm.action.ActivatePaneDirection 'Right',
-  },
-}
+    action = wezterm.action.ActivatePaneDirection 'Right'
+}}
 
 return config
